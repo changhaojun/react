53f464b1e9270e39b602429228872084db0b57bf
changhaojun648859757@qq.com <changhaojun648859757@qq.com>
2019-04-23 17:38:34 +0800

    更改router文件

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './router/App';
+import Router from './router/router';
 import * as serviceWorker from './serviceWorker';
 import './assets/styles/reset.scss';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Router />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
